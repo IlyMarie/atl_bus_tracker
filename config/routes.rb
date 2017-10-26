0d591b17c784ad97ada08b82bd04d8edfea44ab0
Ilyanette Bernabel <ibernabel212@gmail.com>
2017-10-25 21:07:33 -0500

    Updated styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'find-me-a-bus' => 'locations#new'
   resources :locations
+
 
   root 'locations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
